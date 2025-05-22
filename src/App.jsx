93a5d13890744c081616fd70e54b857b32c9f27d
Yeasir Arafat <ysarafat.bd@gmail.com>
2025-05-23 00:21:12 +0600

    Add Footer component to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import Sidebar from "./components/Sidebar";
 
 export default function App() {
-    return (
-        <>
-        <Header />
-        <main>
-		    <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
-        <Sidebar />
-        <MovieList />
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+          <MovieList />
         </div>
-        </main>
-
-        </>
-    );
+      </main>
+      <Footer />
+    </>
+  );
 }
